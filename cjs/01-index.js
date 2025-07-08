3ffcc83c3ebe83b3d18565b1df9f9c65dd175633
Richi <ricardo0640@outlook.com>
2025-07-08 14:14:19 -0600

    esta es la nueva carpeta cjs donde iran los archivos: 01-index.js y 02-sum.js

---
+++ cjs/01-index.js
@@ -0,0 +1,12 @@
+// File: 01-index.js 
+// CommonJS require module:                                                       
+const {sum} = require('./02-sum.js');
+
+
+console.log(sum(2, 3));
+
+
+
+
+
+
