bcedb406d9a12c5df567c40f0365559bd7b928e7
Cr1stopher636 <ricardo0640@outlook.com>
2025-09-08 16:30:04 -0600

    files: update

---
+++ fronted/index.js
@@ -0,0 +1,7 @@
+var persona = {
+    nombre: "pedro",
+    edad: 22,
+    casado: false,
+    nacionalidad: "Europea"
+};
+console.log(persona);
---
+++ fronted/index.ts
@@ -0,0 +1,18 @@
+type persona = {
+    nombre: string;
+    edad: number;
+    casado: boolean;
+    nacionalidad: string;
+}
+
+let persona: persona = {
+    nombre: "pedro",
+    edad: 22,
+    casado: false,
+    nacionalidad: "Europea"
+}
+
+console.log(persona);
+
+
+
